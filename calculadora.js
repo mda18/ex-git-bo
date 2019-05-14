e5eff6e50de1c47c0f9be1973bc962849ab5618d
mda18 <martada2697@gmail.com>
2019-05-14 10:44:27 +0200

    noooouuu

--- calculadora.js
+++ calculadora.js
@@ -1,16 +1,13 @@
 function eventoClickOperacion(){
+var num1= prompt("Numero 1")
+var num2= prompt("Numero 2");
+num1= Number(num1);
+num2= Number(num2);
+var suma= num1+num2;
+
+document.write("<button>"+num1+"+"+num2+"="+suma+"</button>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+eventoClickOperacion();
 }
